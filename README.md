11bbab68a65d3c57a71527333c90877fd0ce4709
Rob Ellis <rellis@slack-corp.com>
2016-11-29 21:26:35 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Message Object for SuperScript
---
+++ src/util.js
@@ -0,0 +1,24 @@
+const genId = function genId() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < 8; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const cleanMessage = function cleanMessage(message) {
+  message = message.replace(/\./g, ' ');
+  message = message.replace(/\s,\s/g, ' ');
+  // these used to be bursted but are not anymore.
+  message = message.replace(/([a-zA-Z]),\s/g, '$1 ');
+  message = message.replace(/"(.*)"/g, '$1');
+  message = message.replace(/\s"\s?/g, ' ');
+  message = message.replace(/\s'\s?/g, ' ');
+  message = message.replace(/\s?!\s?/g, ' ');
+  message = message.replace(/\?\s?/g, ' ');
+  return message;
+};
+
+export default {cleanMessage, genId};
