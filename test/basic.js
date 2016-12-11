6aba6aeba7b98638aafcf0df21bf157e7f3dfe13
Ben James <benhjames@sky.com>
2016-12-11 22:34:36 +0000

    Replace should with should/as-function

--- test/basic.js
+++ test/basic.js
@@ -1,7 +1,7 @@
 /* global describe, it */
 
 import mocha from 'mocha';
-import should from 'should';
+import should from 'should/as-function';
 
 import Message from '../src';
 
@@ -10,47 +10,47 @@ describe('Message Interface', () => {
 
   it('message basics', (done) => {
     Message.createMessage('This `is` a test, yes a test!', {}, (err, mo) => {
-      mo.id.should.be.instanceof(String).and.have.lengthOf(8);
-      mo.createdAt.should.be.instanceof(Date);
-      mo.original.should.be.instanceof(String);
-      mo.raw.should.be.instanceof(String);
-      mo.clean.should.be.instanceof(String);
+      should(mo.id).be.instanceof(String).and.have.lengthOf(8);
+      should(mo.createdAt).be.instanceof(Date);
+      should(mo.original).be.instanceof(String);
+      should(mo.raw).be.instanceof(String);
+      should(mo.clean).be.instanceof(String);
       done();
     });
   });
 
   it('Should call plugin', (done) => {
     Message.createMessage('test', { pluginsPath }, (err, mo) => {
-      mo.plugins.should.be.instanceof(Object).and.have.key('qm');
-      mo.prop.should.equal('test???');
+      should(mo.plugins).be.instanceof(Object).and.have.key('qm');
+      should(mo.prop).equal('test???');
       done();
     });
   });
 
   it('Should contain tags', (done) => {
     Message.createMessage('Hello friend', {}, (err, mo) => {
-      mo.tags.should.be.eql(['hello']);
+      should(mo.tags).be.eql(['hello']);
       done();
     });
   });
 
   it('Numbers should be in numeric form', (done) => {
     Message.createMessage('what is one plus twenty-one', {}, (err, mo) => {
-      mo.clean.should.be.eql('what is 1 plus 21');
+      should(mo.clean).be.eql('what is 1 plus 21');
       done();
     });
   });
 
   it('should find Date Obj', (done) => {
     Message.createMessage('If I was born on February 23 1980 how old am I', {}, (err, mo) => {
-      mo.dates.should.not.be.empty;
+      should(mo.dates).not.be.empty;
       done();
     });
   });
 
   it('Should fetch entities', (done) => {
     Message.createMessage('Rob and Heather know Ashley and Brooklyn', {}, (err, mo) => {
-      mo.entities.should.not.be.empty;
+      should(mo.entities).not.be.empty;
       // This still needs some work.
       // mo.entities.should.be.eql(['Rob', 'Heather', 'Ashley', 'Brooklyn']);
       done();
@@ -59,18 +59,18 @@ describe('Message Interface', () => {
 
   it('Should pull out parts-of-speech', (done) => {
     Message.createMessage('I like the finner things.', {}, (err, mo) => {
-      mo.nouns.should.not.be.empty;
-      mo.pronouns.should.not.be.empty;
-      mo.verbs.should.not.be.empty;
+      should(mo.nouns).not.be.empty;
+      should(mo.pronouns).not.be.empty;
+      should(mo.verbs).not.be.empty;
       done();
     });
   });
 
   it('Should pull out parts-of-speech 2', (done) => {
     Message.createMessage('She ran to Vancouver', {}, (err, mo) => {
-      mo.nouns.should.have.lengthOf(1);
-      mo.pronouns.should.have.lengthOf(1);
-      mo.verbs.should.have.lengthOf(1);
+      should(mo.nouns).have.lengthOf(1);
+      should(mo.pronouns).have.lengthOf(1);
+      should(mo.verbs).have.lengthOf(1);
       done();
     });
   });
@@ -78,37 +78,37 @@ describe('Message Interface', () => {
 
   it('singalize', (done) => {
     Message.createMessage('i love shoes', {}, (err, mo) => {
-      mo.lemString.should.eql('i love shoe');
+      should(mo.lemString).eql('i love shoe');
       done();
     });
   });
 
   it('edge case 1', (done) => {
     Message.createMessage('okay my name is Adam', {}, (err, mo) => {
-      mo.clean.should.eql('okay my name is Adam');
+      should(mo.clean).eql('okay my name is Adam');
       done();
     });
   });
 
   it('edge case 2', (done) => {
     Message.createMessage('yes it is the capital of spain!', {}, (err, mo) => {
-      mo.clean.should.eql('yes it is the capital of spain');
+      should(mo.clean).eql('yes it is the capital of spain');
       done();
     });
   });
 
   it('Is Question', (done) => {
     Message.createMessage('test', {}, (err, mo) => {
-      mo.isQuestion.should.be.false();
+      should(mo.isQuestion).be.false();
 
       Message.createMessage('test ?', {}, (err, mo) => {
-        mo.isQuestion.should.be.true();
+        should(mo.isQuestion).be.true();
 
         Message.createMessage('what is a test', {}, (err, mo) => {
-          mo.isQuestion.should.be.true();
+          should(mo.isQuestion).be.true();
 
           Message.createMessage('do you think this is a test', {}, (err, mo) => {
-            mo.isQuestion.should.be.true();
+            should(mo.isQuestion).be.true();
             done();
           });
         });
@@ -121,8 +121,8 @@ describe('Message Interface', () => {
     // FIXME: Currently returning [ 'Heather', 'Sydney', 'Rob Ellis', 'Ashley Brooklyn' ]
     it('should parse names and nouns from message 1', (done) => {
       Message.createMessage('Rob Ellis and Heather know Ashley, Brooklyn and Sydney.', { factSystem }, (mo) => {
-        mo.names.should.be.instanceof(Array).and.have.lengthOf(5);
-        mo.nouns.should.be.instanceof(Array).and.have.lengthOf(6);
+        should(mo.names).be.instanceof(Array).and.have.lengthOf(5);
+        should(mo.nouns).be.instanceof(Array).and.have.lengthOf(6);
         done();
       });
     });
@@ -130,59 +130,59 @@ describe('Message Interface', () => {
     // FIXME: Some tests are skipped because 'Concepts' no longer exists: this needs looking at
     it("should parse names and nouns from message 2 - this pulls names from scripted concepts since they are not NNP's", (done) => {
       Message.createMessage('heather knows Ashley, brooklyn and sydney.', { factSystem }, (mo) => {
-        mo.names.should.be.instanceof(Array).and.have.lengthOf(4);
+        should(mo.names).be.instanceof(Array).and.have.lengthOf(4);
         done();
       });
     });
 
     it('should parse names and nouns from message 3 - some NN NN should burst', (done) => {
       Message.createMessage('My friend steve likes to play tennis', { factSystem }, (mo) => {
-        mo.nouns.should.be.instanceof(Array).and.have.lengthOf(3);
-        mo.names.should.be.instanceof(Array).and.have.lengthOf(1);
+        should(mo.nouns).be.instanceof(Array).and.have.lengthOf(3);
+        should(mo.names).be.instanceof(Array).and.have.lengthOf(1);
         done();
       });
     });
 
     it('should have nouns with names filters out (cNouns)', (done) => {
       Message.createMessage('My friend Bob likes to play tennis', { factSystem }, (mo) => {
-        mo.nouns.should.be.instanceof(Array).and.have.lengthOf(3);
-        mo.names.should.be.instanceof(Array).and.have.lengthOf(1);
-        mo.cNouns.should.be.instanceof(Array).and.have.lengthOf(2);
+        should(mo.nouns).be.instanceof(Array).and.have.lengthOf(3);
+        should(mo.names).be.instanceof(Array).and.have.lengthOf(1);
+        should(mo.cNouns).be.instanceof(Array).and.have.lengthOf(2);
         done();
       });
     });
 
     it('should find compare', (done) => {
       Message.createMessage('So do you like dogs or cats.', { factSystem }, (mo) => {
-        mo.questionType.should.eql('CH');
+        should(mo.questionType).eql('CH');
         done();
       });
     });
 
     it('should find compare words 2', (done) => {
       Message.createMessage('What is bigger a dog or cat?', { factSystem }, (mo) => {
-        mo.questionType.should.eql('CH');
+        should(mo.questionType).eql('CH');
         done();
       });
     });
 
     it('should find context', (done) => {
       Message.createMessage('They are going on holidays', { factSystem }, (mo) => {
-        mo.pnouns.should.containEql('they');
+        should(mo.pnouns).containEql('they');
         done();
       });
     });
 
     it('should find expression', (done) => {
       Message.createMessage('one plus one = two', { factSystem }, (mo) => {
-        mo.numericExp.should.be.true;
+        should(mo.numericExp).be.true;
         done();
       });
     });
 
     it('should find Concepts', (done) => {
       Message.createMessage('tell that bitch to fuck off', { factSystem }, (mo) => {
-        mo.sentiment.should.eql(-7);
+        should(mo.sentiment).eql(-7);
         done();
       });
     });
